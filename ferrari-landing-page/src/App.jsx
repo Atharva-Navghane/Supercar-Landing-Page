b3f40fb15c32aa46818ae211861b8fe4fb02bd5e
Atharva-Navghane <atharva.navghane@gmail.com>
2023-08-19 16:47:50 +0530

    Added Information Component to the website

--- ferrari-landing-page/src/App.jsx
+++ ferrari-landing-page/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import InfoComp from './components/InfoComp';
 import NavbarComp from './components/NavbarComp';
 
 function App() {
   return (
     <div>
       <NavbarComp />
-      
+      <InfoComp />
     </div>
   );
 }
