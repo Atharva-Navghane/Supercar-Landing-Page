261f570e7d010087746460199d0880dd4a1adc09
Atharva-Navghane <atharva.navghane@gmail.com>
2023-08-19 16:48:22 +0530

    Information Component of the Website

---
+++ ferrari-landing-page/src/components/InfoComp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './InfoCSS.css';
+function InfoComp()
+{
+    return(
+        <div className="InfoContainer">
+            <p className="InfoText"><h2>Welcome to the world of LUXURY!</h2></p>
+            <p className="InfoText InfoRed"><h2>Welcome to FERRARI</h2></p>
+            <p className="InfoText InfoDescription">Introducing the epitome of automotive excellence - Ferrari. With a legacy spanning over seven decades, Ferrari stands as a true icon of passion, innovation, and unparalleled performance in the world of luxury automobiles. From the sleek lines of its meticulously crafted vehicles to the heart-pounding roar of its engines, each Ferrari is a masterpiece that embodies the perfect fusion of art and engineering. With a commitment to pushing boundaries, Ferrari has consistently delivered cutting-edge technology, delivering unrivaled driving experiences that ignite the senses and elevate automotive enthusiasts to new heights of exhilaration. Experience the thrill of pure speed, the allure of Italian elegance, and the prestige of a globally celebrated brand. Join the elite ranks of Ferrari aficionados and be part of a legacy that continues to define the very essence of automotive magnificence. This is more than a car; this is Ferrari.</p>
+        </div>
+    )
+}
+
+export default InfoComp
