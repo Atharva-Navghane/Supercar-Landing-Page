c4c8c0b34433db954b93470179838bc2aea317b1
Atharva-Navghane <atharva.navghane@gmail.com>
2023-08-19 19:13:35 +0530

    Footer Component for Website

---
+++ ferrari-landing-page/src/components/FooterComp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './NavbarCSS.css';
+
+function FooterComp() {
+  return (
+    <div className="FooterClass">
+      <h5>Made with love by Atharva Navghane</h5>
+    </div>
+  );
+}
+
+export default FooterComp;
